3517b71f17b946425bc55cc8999741c23eee4f69
unknown <alexandre232003@hotmail.com>
2024-01-29 19:47:59 +0100

    Ajout des pages

---
+++ src/pages/Error.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ErrorComponent from "../components/error/ErrorComponent";
+
+const Error = () => {
+    return (
+        <div>
+            <main>
+                <ErrorComponent />
+            </main>
+        </div>
+    );
+};
+
+export default Error;
